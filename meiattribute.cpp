e99f937592855bbc17182286e66eca9731c6ba98
Mahtab Ghamsari-Esfahani <mahtab.ghamsari-esfahani@mail.mcgill.ca>
2011-03-04 17:17:21 -0500

    created meiAttribute class

---
+++ meiattribute.cpp
@@ -0,0 +1,27 @@
+/*
+ *  meiattributes.cpp
+ *  libmei
+ *
+ *  Created by Mahtab Ghamsari-Esfahani on 3/4/11.
+ *  Copyright 2011 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "meiattribute.h"
+
+#include <iostream>
+#include <string>
+
+MeiAttribute::MeiAttribute(string name, string value) {
+	this-> name = name; 
+	this-> value = value; 
+	
+}
+
+string MeiAttribute::getName() {
+	return name;
+}
+
+string MeiAttribute::getValue() {
+	return value;
+}
---
+++ meiattribute.h
@@ -0,0 +1,41 @@
+/*
+ *  meiattribute.h
+ *  libmei
+ *
+ *  Created by Mahtab Ghamsari-Esfahani on 3/4/11.
+ *  Copyright 2011 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#ifndef MEIATTRIBUTE_H_
+#define MEIATTRIBUTE_H_
+
+#include <string> 
+
+using std::string;
+
+class MeiElement;
+
+class MeiAttribute
+	{
+	public:
+		MeiAttribute(string name, string value); 
+		
+		string getName(); 
+		string getValue();		
+		
+	private:
+		string name; 
+		string value;
+		string prefix; 
+		
+		MeiElement element(string name);
+		
+	};
+
+
+
+
+
+
+#endif // MEIATTRIBUTE_H_
