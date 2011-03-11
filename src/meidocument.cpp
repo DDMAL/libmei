1c0316dc0f95187ddb8e62ce867d1d6847b2d317
Mahtab Ghamsari-Esfahani <mahtab.ghamsari-esfahani@mail.mcgill.ca>
2011-03-11 15:52:29 -0500

    Create MeiDocument classes with basic functionality

---
+++ src/meidocument.cpp
@@ -0,0 +1,33 @@
+/*
+ *  meidocument.cpp
+ *  libmei
+ *
+ *  Created by Mahtab Ghamsari-Esfahani on 3/11/11.
+ *  Copyright 2011 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "meidocument.h"
+#include "meielement.h"
+
+MeiDocument::MeiDocument(string name, string encoding) {
+	this->docname = name;
+	this->encoding = encoding;
+}
+
+string MeiDocument::getName() {
+	return docname;
+}
+
+string MeiDocument::getEncoding() {
+	return encoding;
+}
+
+void MeiDocument::setEncoding(string encoding) {
+	this->encoding = encoding;
+}
+
+
+vector<MeiElement> MeiDocument::getElements() {
+	return elements; 
+}
---
+++ src/meidocument.h
@@ -0,0 +1,43 @@
+/*
+ *  meidocument.h
+ *  libmei
+ *
+ *  Created by Mahtab Ghamsari-Esfahani on 3/11/11.
+ *  Copyright 2011 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#ifndef MEIDOCUMENT_H_
+#define MEIDOCUMENT_H_
+
+#include <string>
+#include <vector>
+#include "meielement.h"
+
+using std::string;
+using std::vector;
+
+class MeiDocument
+	{
+	public:
+		MeiDocument(string docname, string encoding);
+		
+		string getName();
+		string getEncoding();
+		vector<MeiElement> getElements();
+		
+		void setEncoding(string encoding);
+		
+		void addElement(MeiElement element); 
+		void removeElement(MeiElement element); 
+		
+	private:
+		string docname;
+		string encoding;
+		
+		MeiElement element (string name);
+		vector<MeiElement> elements;
+		
+	};
+
+#endif // MEIDOCUMENT_H_
