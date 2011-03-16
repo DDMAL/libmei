cde7fcb9a1a75603cea13d7be08cedebb8ed96a0
Mahtab Ghamsari-Esfahani <mahtab.ghamsari-esfahani@mail.mcgill.ca>
2011-03-16 13:08:24 -0400

    created exceptions class to take care of mei specific exceptions

---
+++ src/exceptions.h
@@ -0,0 +1,26 @@
+/*
+ *  meiexceptions.h
+ *  libmei
+ *
+ *  Created by Mahtab Ghamsari-Esfahani on 3/16/11.
+ *  Copyright 2011 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#ifndef EXCEPTIONS_H_
+#define EXCEPTIONS_H_
+
+#include <exception>
+
+using std::exception;
+
+class invalidAttribute: public exception 
+	{
+		virtual const char* what() const throw() {
+			return "This note does not contain a pitch attribute";
+		}
+	};
+
+
+
+#endif //EXCEPTIONS_H_
