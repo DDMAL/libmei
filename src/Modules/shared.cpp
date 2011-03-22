6c941d7fb88e25e77ebeed7ad2975c49ac4c17f8
Mahtab Ghamsari-Esfahani <mahtab.ghamsari-esfahani@mail.mcgill.ca>
2011-03-22 16:00:39 -0400

    added all empty sub-classes under shared.

--- src/Modules/shared.cpp
+++ src/Modules/shared.cpp
@@ -10,6 +10,126 @@
 #include "shared.h"
 #include "exceptions.h"
 
+Abbr::Abbr(): MeiElement("abbr") {
+}
+
+Accid::Accid(): MeiElement("accid") {
+}
+
+Address::Address(): MeiElement("address") {
+}
+
+AddressLine::AddressLine(): MeiElement("addressline") {
+}
+
+Annot::Annot(): MeiElement("annot") {
+}
+
+AppInfo::AppInfo(): MeiElement("appinfo") {
+}
+
+Artic::Artic(): MeiElement("artic") {
+}
+
+BarLine::BarLine(): MeiElement("barline") {
+}
+
+Bibl::Bibl(): MeiElement("bibl") {
+}
+
+Body::Body(): MeiElement("body") {
+}
+
+Caption::Caption(): MeiElement("caption") {
+}
+
+Chord::Chord(): MeiElement("chord") {
+}
+
+Clef::Clef(): MeiElement("clef") {
+}
+
+ClefChange::ClefChange(): MeiElement("clefchange") {
+}
+
+Custos::Custos(): MeiElement("custos") {
+}
+
+Date::Date(): MeiElement("date") {
+}
+
+Dir::Dir(): MeiElement("dir") {
+}
+
+Dot::Dot(): MeiElement("dot") {
+}
+
+Dynam::Dynam(): MeiElement("dynam") {
+}
+
+Edition::Edition(): MeiElement("edition") {
+}
+
+Ending::Ending(): MeiElement("ending") {
+}
+
+Expan::Expan(): MeiElement("expan") {
+}
+
+Expansion::Expansion(): MeiElement("expansion") {
+}
+
+Fw::Fw(): MeiElement("fw") {
+}
+
+Group::Group(): MeiElement("group") {
+}
+
+Grpsym::Grpsym(): MeiElement("grpsym") {
+}
+
+Identifier::Identifier(): MeiElement("identifier") {
+}
+
+Instrdef::Instrdef(): MeiElement("instrdef") {
+}
+
+Instrgrp::Instrgrp(): MeiElement("instrgrp") {
+}
+
+KeyAccid::KeyAccid(): MeiElement("keyaccid") {
+}
+
+KeyChange::KeyChange(): MeiElement("keychange") {
+}
+
+KeySig::KeySig(): MeiElement("keysig") {
+}
+
+Label::Label(): MeiElement("label") {
+}
+
+Layer::Layer(): MeiElement("layer") {
+}
+
+LayerDef::LayerDef(): MeiElement("layerdef") {
+}
+
+Lb::Lb(): MeiElement("lb") {
+}
+
+Mdiv::Mdiv(): MeiElement("mdiv") {
+}
+
+Mei::Mei(): MeiElement("mei") {
+}
+
+Music::Music(): MeiElement("music") {
+}
+
+Name::Name(): MeiElement("name") {
+}
+
 Note::Note(): MeiElement("note") {
 }
 
@@ -26,8 +146,83 @@ void Note::setPitchName(string pitchname) {
 	MeiAttribute pitchName = MeiAttribute("pname", pitchname);
 }
 
-Clef::Clef(): MeiElement("clef") {
+Num::Num(): MeiElement("num") {
 }
 
-KeySig::KeySig(): MeiElement("clef") {
+P::P(): MeiElement("p") {
+}
+
+Pad::Pad(): MeiElement("pad") {
+}
+
+Part::Part(): MeiElement("part") {
+}
+
+Parts::Parts(): MeiElement("parts") {
+}
+
+Pb::Pb(): MeiElement("pb") {
+}
+
+PgDesc::PgDesc(): MeiElement("pgdesc") {
+}
+
+PgFoot1::PgFoot1(): MeiElement("pgfoot1") {
+}
+
+PgFoot2::PgFoot2(): MeiElement("pgfoot2") {
+}
+
+PgHead1::PgHead1(): MeiElement("pghead1") {
+}
+
+PgHead2::PgHead2(): MeiElement("pghead2") {
+}
+
+Phrase::Phrase(): MeiElement("phrase") {
+}
+
+Rend::Rend(): MeiElement("rend") {
+}
+
+Repository::Repository(): MeiElement("repository") {
+}
+
+Rest::Rest(): MeiElement("rest") {
+}
+
+Sb::Sb(): MeiElement("sb") {
+}
+
+Score::Score(): MeiElement("score") {
+}
+
+ScoreDef::ScoreDef(): MeiElement("scoredef") {
+}
+
+Space::Space(): MeiElement("space") {
+}
+
+Stack::Stack(): MeiElement("stack") {
+}
+
+Staff::Staff(): MeiElement("staff") {
+}
+
+StaffDef::StaffDef(): MeiElement("staffdef") {
+}
+
+StaffGrp::StaffGrp(): MeiElement("staffgrp") {
+}
+
+Syl::Syl(): MeiElement("syl") {
+}
+
+Tempo::Tempo(): MeiElement("tempo") {
+}
+
+Title::Title(): MeiElement("title") {
+}
+
+TitlePage::TitlePage(): MeiElement("titlepage") {
 }
--- src/Modules/shared.h
+++ src/Modules/shared.h
@@ -13,6 +13,286 @@
 #include "meiattribute.h"
 #include "exceptions.h"
 
+class Abbr: public MeiElement
+	{
+	public:
+		Abbr();
+	private:
+	};
+
+class Accid: public MeiElement
+	{
+	public:
+		Accid();
+	private:
+	};
+
+class Address: public MeiElement
+	{
+	public:
+		Address();
+	private:
+	};
+
+class AddressLine: public MeiElement
+	{
+	public:
+		AddressLine();
+	private:
+	};
+
+class Annot: public MeiElement
+	{
+	public:
+		Annot();
+	private:
+	};
+
+class AppInfo: public MeiElement
+	{
+	public:
+		AppInfo();
+	private:
+	};
+
+class Artic: public MeiElement
+	{
+	public:
+		Artic();
+	private:
+	};
+
+class BarLine: public MeiElement
+	{
+	public:
+		BarLine();
+	private:
+	};
+
+class Bibl: public MeiElement
+	{
+	public:
+		Bibl();
+	private:
+	};
+
+class Body: public MeiElement
+	{
+	public:
+		Body();
+	private:
+	};
+
+class Caption: public MeiElement
+	{
+	public:
+		Caption();
+	private:
+	};
+
+class Chord: public MeiElement
+	{
+	public:
+		Chord();
+	private:
+	};
+
+class Clef: public MeiElement 
+	{
+	public:
+		Clef();
+	private:
+	};
+
+class ClefChange: public MeiElement
+	{
+	public:
+		ClefChange();
+	private:
+	};
+
+class Custos: public MeiElement
+	{
+	public:
+		Custos();
+	private:
+	};
+
+class Date: public MeiElement
+	{
+	public:
+		Date();
+	private:
+	};
+
+class Dir: public MeiElement
+	{
+	public:
+		Dir();
+	private:
+	};
+
+class Dot: public MeiElement
+	{
+	public:
+		Dot();
+	private:
+	};
+
+class Dynam: public MeiElement
+	{
+	public:
+		Dynam();
+	private:
+	};
+
+class Edition: public MeiElement
+	{
+	public:
+		Edition();
+	private:
+	};
+
+class Ending: public MeiElement
+	{
+	public:
+		Ending();
+	private:
+	};
+
+class Expan: public MeiElement
+	{
+	public:
+		Expan();
+	private:
+	};
+
+class Expansion: public MeiElement
+	{
+	public:
+		Expansion();
+	private:
+	};
+
+class Fw: public MeiElement
+	{
+	public:
+		Fw();
+	private:
+	};
+
+class Group: public MeiElement
+	{
+	public:
+		Group();
+	private:
+	};
+
+class Grpsym: public MeiElement
+	{
+	public:
+		Grpsym();
+	private:
+	};
+
+class Identifier: public MeiElement
+	{
+	public:
+		Identifier();
+	private:
+	};
+
+class Instrdef: public MeiElement
+	{
+	public:
+		Instrdef();
+	private:
+	};
+
+class Instrgrp: public MeiElement
+	{
+	public:
+		Instrgrp();
+	private:
+	};
+
+class KeyAccid: public MeiElement
+	{
+	public:
+		KeyAccid();
+	private:
+	};
+
+class KeyChange: public MeiElement
+	{
+	public:
+		KeyChange();
+	private:
+	};
+
+class KeySig: public MeiElement 
+	{
+	public:
+		KeySig();
+	private:
+	};
+
+class Label: public MeiElement
+	{
+	public:
+		Label();
+	private:
+	};
+
+class Layer: public MeiElement
+	{
+	public:
+		Layer();
+	private:
+	};
+
+class LayerDef: public MeiElement
+	{
+	public:
+		LayerDef();
+	private:
+	};
+
+class Lb: public MeiElement
+	{
+	public:
+		Lb();
+	private:
+	};
+
+class Mdiv: public MeiElement
+	{
+	public:
+		Mdiv();
+	private:
+	};
+
+class Mei: public MeiElement
+	{
+	public:
+		Mei();
+	private:
+	};
+
+class Music: public MeiElement
+	{
+	public:
+		Music();
+	private:
+	};
+
+class Name: public MeiElement
+	{
+	public:
+		Name();
+	private:
+	};
+
 class Note: public MeiElement 
 	{
 	public:
@@ -24,19 +304,200 @@ class Note: public MeiElement
 		string pitchname;
 	};
 
-class Clef: public MeiElement 
+class Num: public MeiElement
 	{
 	public:
-		Clef();
+		Num();
 	private:
 	};
 
-class KeySig: public MeiElement 
+class P: public MeiElement
 	{
 	public:
-		KeySig();
+		P();
 	private:
 	};
 
+class Pad: public MeiElement
+	{
+	public:
+		Pad();
+	private:
+	};
+
+class Part: public MeiElement
+	{
+	public:
+		Part();
+	private:
+	};
+
+class Parts: public MeiElement
+	{
+	public:
+		Parts();
+	private:
+	};
+
+class Pb: public MeiElement
+	{
+	public:
+		Pb();
+	private:
+	};
+
+class PgDesc: public MeiElement
+	{
+	public:
+		PgDesc();
+	private:
+	};
+
+class PgFoot1: public MeiElement
+	{
+	public:
+		PgFoot1();
+	private:
+	};
+
+class PgFoot2: public MeiElement
+	{
+	public:
+		PgFoot2();
+	private:
+	};
+
+class PgHead1: public MeiElement
+	{
+	public:
+		PgHead1();
+	private:
+	};
+
+class PgHead2: public MeiElement
+	{
+	public:
+		PgHead2();
+	private:
+	};
+
+class Phrase: public MeiElement
+	{
+	public:
+		Phrase();
+	private:
+	};
+
+class Rend: public MeiElement
+	{
+	public:
+		Rend();
+	private:
+	};
+
+class Repository: public MeiElement
+	{
+	public:
+		Repository();
+	private:
+	};
+
+class Rest: public MeiElement
+	{
+	public:
+		Rest();
+	private:
+	};
+
+class Sb: public MeiElement
+	{
+	public:
+		Sb();
+	private:
+	};
+
+class Score: public MeiElement
+	{
+	public:
+		Score();
+	private:
+	};
+
+class ScoreDef: public MeiElement
+	{
+	public:
+		ScoreDef();
+	private:
+	};
+
+class Section: public MeiElement
+	{
+	public:
+		Section();
+	private:
+	};
+
+class Space: public MeiElement
+	{
+	public:
+		Space();
+	private:
+	};
+
+class Stack: public MeiElement
+	{
+	public:
+		Stack();
+	private:
+	};
+
+class Staff: public MeiElement
+	{
+	public:
+		Staff();
+	private:
+	};
+
+class StaffDef: public MeiElement
+	{
+	public:
+		StaffDef();
+	private:
+	};
+
+class StaffGrp: public MeiElement
+	{
+	public:
+		StaffGrp();
+	private:
+	};
+
+class Syl: public MeiElement
+	{
+	public:
+		Syl();
+	private:
+	};
+
+class Tempo: public MeiElement
+	{
+	public:
+		Tempo();
+	private:
+	};
+
+class Title: public MeiElement
+	{
+	public:
+		Title();
+	private:
+	};
+
+class TitlePage: public MeiElement
+	{
+	public:
+		TitlePage();
+	private:
+	};
 
 #endif // SHARED_H_
