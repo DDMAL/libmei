de06c0bb480ebfb4bfcf7774ed41ca0ae5db76d9
Andrew Hankinson <andrew.hankinson@gmail.com>
2011-08-17 18:59:34 -0400

    Adding the new test harness.

--- util/runmei.cpp
+++ util/runmei.cpp
@@ -7,65 +7,97 @@
 //
 
 #include <iostream>
+#include <mei/shared.h>
+#include <mei/meielement.h>
+#include <gtest/gtest.h>
 
+using std::cout;
 using std::string;
 
-class CoolMixIn {
-public:
-    CoolMixIn(ParentClass *b);
-    virtual ~CoolMixIn() {};
+using mei::MeiElement;
+using mei::MeiFactory;
+using mei::Note;
+
+// class ParentClass {
+// public:
+//     ParentClass(string name);
+//     virtual ~ParentClass();
+//     
+//     string getName();
+//     
+// private:
+//     string name;
+// };
+// 
+// ParentClass::ParentClass(string name) {
+//     this->name = name;
+// }
+// 
+// string ParentClass::getName() {
+//     return this->name;
+// }
+// 
+// ParentClass::~ParentClass() {}
+// 
+// class CoolMixIn {
+// public:
+//     CoolMixIn(ParentClass *b);
+//     virtual ~CoolMixIn() {};
+//     
+//     string getCoolness();
+// private:
+//     ParentClass *b;
+// };
+// 
+// CoolMixIn::CoolMixIn(ParentClass *b) {
+//     this->b = b;
+// }
+// 
+// string CoolMixIn::getCoolness() {
+//     return this->b->getName();
+// }
+// 
+// 
+// class ChildClass : public ParentClass {
+// public:
+//     ChildClass();
+//     virtual ~ChildClass() {};
+//     
+//     CoolMixIn m_Cool;
+// private:
+// };
+// 
+// ChildClass::ChildClass() :
+//     ParentClass("so cool"),
+//     m_Cool(this)
+// {
+//     
+// }
+
+// int main(int argc, char **argv) {
+TEST(XYTest, NewXYTest) {
+    // ParentClass *p = new ChildClass();
+    // 
+    // string coolness = dynamic_cast<ChildClass*>(p)->m_Cool.getCoolness();
+    // 
+    // cout << "how cool? " << coolness << "\n";
+    MeiElement *e = MeiFactory::createInstance("note");
     
-    string getCoolness();
-private:
-    ParentClass *b;
-}
-
-CoolMixIn::CoolMixIn(ParentClass *b) {
-    this->b = b;
-}
-
-string CoolMixIn::getCoolness() {
-    return this->b.getName();
-}
-
-
-class ParentClass {
-public:
-    ParentClass(string name);
-    virtual ~ParentClass();
+    string name = e->getName();
+    cout << "I am a " << name << "\n";
     
-    string getName() {
-        return this->name;
+    Note *n = dynamic_cast<Note*>(e);
+    
+    if (n == NULL) {
+        cout << "Sad panda." << "\n";
+    } else {
+        n->m_NoteVis.setHeadshape("diamond");
+        
+        cout << "I have a head shaped like a " << n->m_NoteVis.getHeadshape()->getValue() << "\n";
     }
+    dynamic_cast<Note*>(e)->m_NoteVis.removeHeadshape();
+    dynamic_cast<Note*>(e)->m_NoteVis.setHeadshape("XXX");
+    cout << "New head " << dynamic_cast<Note*>(e)->m_NoteVis.getHeadshape()->getValue() << "\n";
     
-private:
-    string name;
-}
-
-class ChildClass : public ParentClass {
-public:
-    ChildClass();
-    virtual ~ChildClass() {};
-    
-    CoolMixIn m_Cool;
-private:
-}
-
-ChildClass::ChildClass() :
-    ParentClass("so cool"),
-    m_Cool(this)
-{
-    
-}
-
-int main(int argc, char **argv) {
-    
-    ParentClass *p = new ChildClass();
-    
-    string coolness = dynamic_cast<ChildClass*>(p)->getCoolness();
-    
-    cout << "how cool? " << coolness << "\n";
-    
-    
-    return 0;
+    // return 0;
 }
