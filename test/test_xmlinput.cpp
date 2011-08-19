e5270c5c59735b5fd248fcf4192d46081f478c5e
Andrew Hankinson <andrew.hankinson@mail.mcgill.ca>
2011-08-18 23:09:58 -0400

    Adding  test cases and an MEI file for testing

---
+++ test/test_xmlinput.cpp
@@ -0,0 +1,21 @@
+//
+//  test_xmlinput.cpp
+//  libmei
+//
+//  Created by Andrew Hankinson on 11-08-18.
+//  Copyright 2011 McGill University. All rights reserved.
+//
+
+#include <gtest/gtest.h>
+#include <iostream>
+#include <mei/xmlimport.h>
+#include <mei/meielement.h>
+#include <mei/meidocument.h>
+
+using mei::MeiDocument;
+
+
+TEST(TestMeiXmlImport, ReadFileIn) {
+    MeiDocument* docf = mei::XmlImport::File("/Users/ahankins/Documents/code/git/libmei/test/beethoven.mei");
+    
+}
