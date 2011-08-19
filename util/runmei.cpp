098aa918c222ed54ac219d8093ba43ef35e256f8
Andrew Hankinson <andrew.hankinson@mail.mcgill.ca>
2011-08-18 23:10:28 -0400

    Changes to the runmei scratchpad script

--- util/runmei.cpp
+++ util/runmei.cpp
@@ -1,103 +1,95 @@
-//
-//  runmei.cpp
-//  libmei
-//
-//  Created by Andrew Hankinson on 11-08-14.
-//  Copyright 2011 McGill University. All rights reserved.
-//
+/**
+ * section: xmlReader
+ * synopsis: Parse an XML file with an xmlReader
+ * purpose: Demonstrate the use of xmlReaderForFile() to parse an XML file
+ *          and dump the informations about the nodes found in the process.
+ *          (Note that the XMLReader functions require libxml2 version later
+ *          than 2.6.)
+ * usage: reader1 <filename>
+ * test: reader1 test2.xml > reader1.tmp ; diff reader1.tmp reader1.res ; rm reader1.tmp
+ * author: Daniel Veillard
+ * copy: see Copyright for the status of this software.
+ */
 
-#include <iostream>
-#include <mei/shared.h>
-#include <mei/meielement.h>
-#include <gtest/gtest.h>
+#include <iostream.h>
+#include <stdio.h>
+#include <libxml/xmlreader.h>
+#include <mei/xmlimport.h>
+#include <mei/meidocument.h>
 
-using std::cout;
 using std::string;
+using std::cout;
+using mei::MeiDocument;
 
-using mei::MeiElement;
-using mei::MeiFactory;
-using mei::Note;
+//static void XmlNodeToMei(xmlNode* node) {
+//    xmlAttr* currattr = NULL;
+//	xmlNode* currnode = NULL;
+//    xmlNode* attrvalue = NULL;
+//    
+//    for (currnode = node; currnode; currnode = currnode->next) {
+//        if (currnode->type == XML_ELEMENT_NODE) {
+//            cout << "Node: " << currnode->name << endl;
+//            if(currnode->properties) {
+//                for (currattr = currnode->properties; currattr; currattr = currattr->next) {
+//                    attrvalue = currattr->children;
+//                    cout << "  Attr: " << currattr->name << ": " << attrvalue->content << endl;
+//                }
+//            }
+//            
+//            
+//            XmlNodeToMei(currnode->children);
+//            
+//            
+//            
+//        } else if (currnode->type == XML_TEXT_NODE) {
+//            cout << "Text node: " << currnode->content << endl;
+//        } else {
+//            cout << "unknown node type " << currnode->name << endl;
+//            cout << "   of type " << currnode->type << endl;
+//        }
+////            MeiElement* child = MeiFactory::createInstanceFromNode(string((const char*)curnode->name),curnode);
+////            if (child) {
+////                XmlNodeToMei(curnode->children, child);
+////                parent->addChild(child);
+////            } else {
+////                string message = "unknown node type " + (string)(const char*)curnode->name;
+////                throw message;
+////            }            
+//	}
+//}
 
-// class ParentClass {
-// public:
-//     ParentClass(string name);
-//     virtual ~ParentClass();
-//     
-//     string getName();
-//     
-// private:
-//     string name;
-// };
-// 
-// ParentClass::ParentClass(string name) {
-//     this->name = name;
-// }
-// 
-// string ParentClass::getName() {
-//     return this->name;
-// }
-// 
-// ParentClass::~ParentClass() {}
-// 
-// class CoolMixIn {
-// public:
-//     CoolMixIn(ParentClass *b);
-//     virtual ~CoolMixIn() {};
-//     
-//     string getCoolness();
-// private:
-//     ParentClass *b;
-// };
-// 
-// CoolMixIn::CoolMixIn(ParentClass *b) {
-//     this->b = b;
-// }
-// 
-// string CoolMixIn::getCoolness() {
-//     return this->b->getName();
-// }
-// 
-// 
-// class ChildClass : public ParentClass {
-// public:
-//     ChildClass();
-//     virtual ~ChildClass() {};
-//     
-//     CoolMixIn m_Cool;
-// private:
-// };
-// 
-// ChildClass::ChildClass() :
-//     ParentClass("so cool"),
-//     m_Cool(this)
-// {
-//     
-// }
 
-// int main(int argc, char **argv) {
-TEST(XYTest, NewXYTest) {
-    // ParentClass *p = new ChildClass();
-    // 
-    // string coolness = dynamic_cast<ChildClass*>(p)->m_Cool.getCoolness();
-    // 
-    // cout << "how cool? " << coolness << "\n";
-    MeiElement *e = MeiFactory::createInstance("note");
+int main(int argc, char **argv) {
+//    if (argc != 2)
+//        return(1);
     
-    string name = e->getName();
-    cout << "I am a " << name << "\n";
+    mei::MeiDocument *d = mei::XmlImport::File("/Users/ahankins/Documents/code/git/libmei/test/beethoven.mei");
+//    MeiDocument *d = mei::XmlImport::File(argv[1]);
     
-    Note *n = dynamic_cast<Note*>(e);
+//    xmlDoc *doc = NULL;
+//	xmlNode *rootelement = NULL;
+//    xmlAttr* rootattr = NULL;
+//    const xmlChar* attrname;
+//    xmlNode* attrvalue = NULL;
+//    xmlNs* rootxmlns = NULL;
+//	
+//    doc = xmlReadFile(argv[1], NULL, 0);
+//	rootelement = xmlDocGetRootElement(doc);
+//    
+//    rootxmlns = rootelement->ns; 
+//	const xmlChar* roothref = rootxmlns->href;
+//	const xmlChar* rootprefix = rootxmlns->prefix;
     
-    if (n == NULL) {
-        cout << "Sad panda." << "\n";
-    } else {
-        n->m_NoteVis.setHeadshape("diamond");
-        
-        cout << "I have a head shaped like a " << n->m_NoteVis.getHeadshape()->getValue() << "\n";
-    }
-    dynamic_cast<Note*>(e)->m_NoteVis.removeHeadshape();
-    dynamic_cast<Note*>(e)->m_NoteVis.setHeadshape("XXX");
-    cout << "New head " << dynamic_cast<Note*>(e)->m_NoteVis.getHeadshape()->getValue() << "\n";
+//    XmlNodeToMei(rootelement->children);
+//    
+//    /*
+//     * Cleanup function for the XML library.
+//     */
+//    xmlCleanupParser();
+//    /*
+//     * this is to debug memory for regression tests
+//     */
+//    xmlMemoryDump();
     
-    // return 0;
+    return 0;
 }
