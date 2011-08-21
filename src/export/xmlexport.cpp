482cb11fa5dfeb3e60ad9fb4329893cd852b9d3e
Andrew Hankinson <andrew.hankinson@mail.mcgill.ca>
2011-08-21 13:40:04 -0400

    Initial work on XML Exporter

--- src/export/xmlexport.cpp
+++ src/export/xmlexport.cpp
@@ -7,3 +7,46 @@
 //
 
 #include <iostream>
+#include <string.h>
+#include <stdio.h>
+#include <vector>
+#include <libxml/xmlwriter.h>
+
+#include "xmlexport.h"
+#include "meidocument.h"
+
+using std::string;
+using std::cout;
+using std::endl;
+
+using mei::MeiDocument;
+using mei::MeiElement;
+using mei::MeiFactory;
+using mei::XmlExport;
+
+void XmlExport::meiDocumentToFile(mei::MeiDocument *doc, const char *filename) {
+    
+}
+
+XmlExport::XmlExport(MeiDocument *doc) {
+    //constructor
+}
+
+XmlExport::~XmlExport() {}
+
+void XmlExport::init() {}
+
+xmlNode* XmlExport::meiElementToXmlNode(MeiElement *el) {
+    
+    
+    if (el->getChildren().size() > 0) {
+        vector<MeiElement*> cn = el->getChildren();
+        for (vector<MeiElement*>::iterator iter = cn.begin(); iter != cn.end(); ++iter) {
+            // add children to xml node
+            meiElementToXmlNode(*iter);
+        }
+    }
+    
+    return;
+}
+
--- src/export/xmlexport.h
+++ src/export/xmlexport.h
@@ -1,5 +1,5 @@
 //
-//  xmlexport.h
+//  xmlimport.h
 //  libmei
 //
 //  Created by Andrew Hankinson on 11-08-18.
@@ -9,8 +9,25 @@
 #ifndef MEIXMLEXPORT_H_
 #define MEIXMLEXPORT_H_
 
+#include "meidocument.h"
+
 namespace mei {
     
+    class XmlExport {
+    public:
+        /** public interfaces for importing. Each of these will convert their input
+         *  into an xmlNode for processing by the _MeiXmlStruct class.
+         */
+        static void meiDocumentToFile(MeiDocument *doc, const char* filename);
+        static void meiDocumentToStream(MeiDocument *doc, const char* filename); // figure out arg type!
+        static void meiDocumentToText(MeiDocument *doc); // is this right???
+        
+    private:
+        XmlExport(MeiDocument *doc);
+        virtual ~XmlExport();
+        void init();
+        xmlNode* meiElementToXmlNode(MeiElement *el);
+    };
 }
 
 
