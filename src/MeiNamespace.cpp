1d1cb12c03258445b40f2d17777a93b5c4098a0c
Andrew Hankinson <andrew.hankinson@gmail.com>
2011-08-22 20:20:50 -0400

    Adding meinamespace to XCode Project

---
+++ src/MeiNamespace.cpp
@@ -0,0 +1,9 @@
+//
+//  MeiNamespace.cpp
+//  libmei
+//
+//  Created by Andrew Hankinson on 11-08-22.
+//  Copyright 2011 McGill University. All rights reserved.
+//
+
+#include <iostream>
---
+++ src/MeiNamespace.h
@@ -0,0 +1,14 @@
+//
+//  MeiNamespace.h
+//  libmei
+//
+//  Created by Andrew Hankinson on 11-08-22.
+//  Copyright 2011 McGill University. All rights reserved.
+//
+
+#ifndef libmei_MeiNamespace_h
+#define libmei_MeiNamespace_h
+
+
+
+#endif
