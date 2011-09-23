1f637587c834bfa3214aeedb610588875cbca9b8
Greg Burlet <gregory.burlet@mail.mcgill.ca>
2011-09-23 12:15:27 -0400

    Added mxmltomei target to the project
    for the standalone executable to convert MusicXML files to MEI. Created main() test stub.

---
+++ util/mxmltomei/mxmltomei.cpp
@@ -0,0 +1,18 @@
+/*
+ * mxmltomei.cpp
+ * musicxmltomei
+ *
+ * Created by Greg Burlet on 11-09-23.
+ * 
+ * Main file for standalone MusicXML to MEI file conversion executable
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main (int argc, const char * argv[]) {
+    cout << "test" << endl;
+    return 0;
+}
+
