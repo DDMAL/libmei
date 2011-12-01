fc330eb5ab1fd446db46fd3152221f1c6c3615ea
Andrew Hankinson <andrew.hankinson@gmail.com>
2011-12-01 01:50:02 -0500

    It builds! And works! Huzzah!
    Next step: Write tests

--- python/pymei.cpp
+++ python/pymei.cpp
@@ -19,11 +19,11 @@ using mei::MeiNamespace;
 using mei::MeiElement;
 using mei::MeiAttribute;
 using mei::MeiDocument;
-using mei::XmlImport;
 
 typedef vector<MeiElement*> MeiElementList;
 typedef vector<MeiAttribute*> MeiAttributeList;
 // typedef vector<MeiElement*>& MeiElementListRef;
+
 
 
 BOOST_PYTHON_MODULE(pymei) {
@@ -34,7 +34,6 @@ BOOST_PYTHON_MODULE(pymei) {
     class_<MeiAttributeList>("MeiAttributeList")
         .def(vector_indexing_suite<MeiAttributeList>())
     ;
-
 
     // class_<MeiElementListRef>("MeiElementListRef")
     //     .def(vector_indexing_suite<MeiElementListRef>())
@@ -47,26 +46,32 @@ BOOST_PYTHON_MODULE(pymei) {
 
     void (MeiElement::*addAttributeByObject)(MeiAttribute*) = &MeiElement::addAttribute;
     void (MeiElement::*addAttributeByString)(string, string) = &MeiElement::addAttribute;
+    void (MeiElement::*setDocument)(MeiDocument*) = &MeiElement::setDocument;
     bool (MeiElement::*hasChildrenBool)() = &MeiElement::hasChildren;
     bool (MeiElement::*hasChildrenArgs)(string) = &MeiElement::hasChildren;
     void (MeiElement::*printAll)() = &MeiElement::print;
     void (MeiElement::*printLvl)(int) = &MeiElement::print;
+    MeiElement* (MeiDocument::*getElementById)(string) = &MeiDocument::getElementById;
 
     class_<MeiDocument>("MeiDocument", init<>())
-    .def("hasNamespace", &MeiDocument::hasNamespace)
-    .def("getNamespace", &MeiDocument::getNamespace, return_value_policy<reference_existing_object>())
-    .def("getNamespaces", &MeiDocument::getNamespaces)
-    .def("addNamespace", &MeiDocument::addNamespace)
-    .def("getVersion", &MeiDocument::getVersion)
-    .def("getRootElement", &MeiDocument::getRootElement, return_value_policy<reference_existing_object>())
-    .def("setRootElement", &MeiDocument::setRootElement)
-    // .def("getElementById", &MeiDocument::getElementById, return_value_policy<manage_new_object>())
-    .def("getElementsByName", &MeiDocument::getElementsByName)
-    .def("getPositionInDocument", &MeiDocument::getPositionInDocument)
-    .def("addIdMap", &MeiDocument::addIdMap)
-    .def("rmIdMap", &MeiDocument::rmIdMap)
-    .def("getFlattenedTree", &MeiDocument::getFlattenedTree, return_value_policy<copy_const_reference>())
-    // .def("lookBack", &MeiDocument::lookBack)
+        .def("hasNamespace", &MeiDocument::hasNamespace)
+        .def("getNamespace", &MeiDocument::getNamespace, return_value_policy<reference_existing_object>())
+        .def("getNamespaces", &MeiDocument::getNamespaces)
+        .def("addNamespace", &MeiDocument::addNamespace)
+        .def("getVersion", &MeiDocument::getVersion)
+        .def("getRootElement", &MeiDocument::getRootElement, return_value_policy<reference_existing_object>())
+        .def("setRootElement", &MeiDocument::setRootElement)
+        /* 
+            MeiDocument has both a private and public function named getElementById. We expose
+            the public one above and use it here.
+        */
+        .def("getElementById", getElementById, return_value_policy<reference_existing_object>())
+        .def("getElementsByName", &MeiDocument::getElementsByName)
+        .def("getPositionInDocument", &MeiDocument::getPositionInDocument)
+        .def("addIdMap", &MeiDocument::addIdMap)
+        .def("rmIdMap", &MeiDocument::rmIdMap)
+        .def("getFlattenedTree", &MeiDocument::getFlattenedTree, return_value_policy<copy_const_reference>())
+        .def("lookBack", &MeiDocument::lookBack, return_value_policy<reference_existing_object>())
 ;
 
     class_<MeiElement>("MeiElement", init<string>())
@@ -85,7 +90,11 @@ BOOST_PYTHON_MODULE(pymei) {
         .def("hasAttribute", &MeiElement::hasAttribute)
         .def("hasParent", &MeiElement::hasParent)
         .def("getParent", &MeiElement::getParent, return_value_policy<reference_existing_object>())
-        .def("setDocument" &MeiElement::setDocument)
+        
+        /* boost doesn't seem to like the forward declaration of MeiDocument in MeiElement.
+            This construction for setDocument avoids this by explicitly masking it above
+        */
+        .def("setDocument", setDocument)
         .def("getDocument", &MeiElement::getDocument, return_value_policy<reference_existing_object>())
         .def("addChild", &MeiElement::addChild)
         .def("setChildren", &MeiElement::setChildren)
@@ -102,7 +111,7 @@ BOOST_PYTHON_MODULE(pymei) {
         .def("getDescendantsByName", &MeiElement::getDescendantsByName)
         .def("getPeers", &MeiElement::getPeers)
         .def("getPositionInDocument", &MeiElement::getPositionInDocument)
-        // .def("lookBack", &MeiElement::lookBack)
+        .def("lookBack", &MeiElement::lookBack, return_value_policy<reference_existing_object>())
         .def("flatten", &MeiElement::flatten)
         .def("print", printAll)
         .def("print", printLvl)
