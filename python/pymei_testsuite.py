9c0d144696a3615197bd5b10c4ebc41aebbb8f02
Andrew Hankinson <andrew.hankinson@gmail.com>
2011-12-04 15:05:04 -0500

    Add export test suite

--- python/pymei_testsuite.py
+++ python/pymei_testsuite.py
@@ -2,12 +2,14 @@ import unittest
 from test import meielement_test
 from test import meidocument_test
 from test import xmlimport_test
+from test import xmlexport_test
 
 def suite():
     test_suite = unittest.TestSuite()
     test_suite.addTest(meielement_test.suite())
     test_suite.addTest(meidocument_test.suite())
     test_suite.addTest(xmlimport_test.suite())
+    test_suite.addTest(xmlexport_test.suite())
     return test_suite
     
 runner = unittest.TextTestRunner()
