f6ed94d3dcd2979a8179b29d96c2656b35e39610
Andrew Hankinson <andrew.hankinson@mail.mcgill.ca>
2012-06-23 10:31:16 -0400

    Adding push() and pop() methods to Python lists

--- python/src/_libmei.cpp
+++ python/src/_libmei.cpp
@@ -87,6 +87,17 @@ string MeiElementList_Print(MeiElementList x) {
     return res.str();
 }
 
+MeiElement* MeiElementList_PopFromList(MeiElementList* x) {
+    MeiElement* t = x->back();
+    x->pop_back();
+    return t;
+}
+
+void MeiElementList_PushToList(MeiElementList* x, MeiElement* y) {
+    x->insert(x->begin(), y);
+}
+
+
 bool MeiAttributeList_EqualWrap(const MeiAttributeList x, const MeiAttributeList y) { return x == y; }
 bool MeiAttributeList_NEqualWrap(const MeiAttributeList x, const MeiAttributeList y) { return x != y; }
 bool MeiAttributeList_NonZero(const MeiAttributeList x) { return !x.empty(); }
@@ -98,6 +109,16 @@ string MeiAttributeList_Print(MeiAttributeList x) {
     }
     res << "]";
     return res.str();
+}
+
+MeiAttribute* MeiAttributeList_PopFromList(MeiAttributeList* x) {
+    MeiAttribute* t = x->back();
+    x->pop_back();
+    return t;
+}
+
+void MeiAttributeList_PushToList(MeiAttributeList* x, MeiAttribute* y) {
+    x->insert(x->begin(), y);
 }
 
 bool MeiNamespaceList_EqualWrap(const MeiNamespaceList x, const MeiNamespaceList y) { return x == y; }
@@ -113,6 +134,17 @@ string MeiNamespaceList_Print(MeiNamespaceList x) {
     return res.str();
 }
 
+MeiNamespace* MeiNamespaceList_PopFromList(MeiNamespaceList* x) {
+    MeiNamespace* t = x->back();
+    x->pop_back();
+    return t;
+}
+
+void MeiNamespaceList_PushToList(MeiNamespaceList* x, MeiNamespace* y) {
+    x->insert(x->begin(), y);
+}
+
+
 string MeiProcessingInstruction_Print(XmlProcessingInstruction x) { return "<XmlProcessingInstruction " + x.getName() + ":" + x.getValue() + ">"; }
 
 string MeiXmlInstructions_Print(XmlInstructions x) {
@@ -124,6 +156,17 @@ string MeiXmlInstructions_Print(XmlInstructions x) {
     res << "]";
     return res.str();
 }
+
+XmlProcessingInstruction* MeiXmlInstructions_PopFromList(XmlInstructions* x) {
+    XmlProcessingInstruction* t = x->back();
+    x->pop_back();
+    return t;
+}
+
+void MeiXmlInstructions_PushToList(XmlInstructions* x, XmlProcessingInstruction* y) {
+    x->insert(x->begin(), y);
+}
+
 
 /*
  * Converter for python lists to vector<T>
@@ -180,6 +223,8 @@ BOOST_PYTHON_MODULE(_libmei) {
         .def(vector_indexing_suite<XmlInstructions>())
         .def("__str__", &MeiXmlInstructions_Print)
         .def("__repr__", &MeiXmlInstructions_Print)
+        .def("pop", &MeiXmlInstructions_PopFromList, return_value_policy<reference_existing_object>())
+        .def("push", &MeiXmlInstructions_PushToList)
     ;
 
     class_<XmlProcessingInstruction, XmlProcessingInstruction*>("XmlProcessingInstruction", init<string, string>())
@@ -197,6 +242,9 @@ BOOST_PYTHON_MODULE(_libmei) {
         .def("__nonzero__", &MeiElementList_NonZero)
         .def("__str__", &MeiElementList_Print)
         .def("__repr__", &MeiElementList_Print)
+        .def("pop", &MeiElementList_PopFromList, return_value_policy<reference_existing_object>())
+        .def("push", &MeiElementList_PushToList)
+
     ;
 
     class_<MeiAttributeList>("MeiAttributeList")
@@ -207,6 +255,8 @@ BOOST_PYTHON_MODULE(_libmei) {
         .def("__nonzero__", &MeiAttributeList_NonZero)
         .def("__str__", &MeiAttributeList_Print)
         .def("__repr__", &MeiAttributeList_Print)
+        .def("pop", &MeiAttributeList_PopFromList, return_value_policy<reference_existing_object>())
+        .def("push", &MeiAttributeList_PushToList)
     ;
 
     class_<MeiNamespaceList>("MeiNamespaceList")
@@ -217,6 +267,8 @@ BOOST_PYTHON_MODULE(_libmei) {
         .def("__nonzero__", &MeiNamespaceList_NonZero)
         .def("__str__", &MeiNamespaceList_Print)
         .def("__repr__", &MeiNamespaceList_Print)
+        .def("pop", &MeiNamespaceList_PopFromList, return_value_policy<reference_existing_object>())
+        .def("push", &MeiNamespaceList_PushToList)
     ;
 
     class_<MeiNamespace, MeiNamespace*>("MeiNamespace", init<string, string>())
