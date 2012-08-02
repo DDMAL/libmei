fb831c45766213434cf99699800fa5b73eb53e90
Andrew Hankinson <andrew.hankinson@gmail.com>
2012-08-02 12:06:56 -0400

    Adding build rules for freebsd

--- python/setup.py
+++ python/setup.py
@@ -10,6 +10,12 @@ if sys.platform == "darwin":
     libraries = ["boost_python-mt"]
     library_dirs = []
     runtime_library_dirs = []
+    include_dirs = []
+elif sys.platform == "freebsd8":
+    libraries = ['boost_python', 'mei']
+    library_dirs = ['/usr/local/lib']
+    runtime_library_dirs = ['/usr/local/lib']
+    include_dirs = ['/usr/local/include']
 else:
     if sys.platform == "linux2":
         import platform
@@ -19,12 +25,14 @@ else:
             ver = platform.python_version_tuple()
             libraries = ["boost_python-mt-py{0}{1}".format(ver[0], ver[1]), "mei"]
             library_dirs = ["/usr/local/lib", "/usr/lib"]
-            runtime_library_dirs=["/usr/local/lib", "/usr/lib"]
+            runtime_library_dirs = ["/usr/local/lib", "/usr/lib"]
+            include_dirs = []
         else:
             libraries = ["boost_python-mt", "mei"]
             library_dirs = ["/usr/local/lib", "/usr/lib"]
-            runtime_library_dirs=["/usr/local/lib", "/usr/lib"]
-    link_args = ["-L/usr/local/lib","-L/usr/lib"]
+            runtime_library_dirs = ["/usr/local/lib", "/usr/lib"]
+            include_dirs = []
+    link_args = ["-L/usr/local/lib", "-L/usr/lib"]
 
 setup(
     name = 'pymei',
@@ -34,18 +42,20 @@ setup(
     include_package_data=True,
     packages=find_packages(),
     ext_modules = [
-        Extension(os.path.join("pymei","_libmei"), 
+        Extension(os.path.join("pymei", "_libmei"),
             [os.path.join("src","_libmei.cpp")],
             libraries=libraries,
             extra_link_args=link_args,
             library_dirs=library_dirs,
+            include_dirs=include_dirs,
             runtime_library_dirs=runtime_library_dirs
         ),
-        Extension(os.path.join("pymei","_libmei_exceptions"), 
+        Extension(os.path.join("pymei","_libmei_exceptions"),
             [os.path.join("src","_libmei_exceptions.cpp")],
             libraries=libraries,
             extra_link_args=link_args,
             library_dirs=library_dirs,
+            include_dirs=include_dirs,
             runtime_library_dirs=runtime_library_dirs
         ),
     ],
