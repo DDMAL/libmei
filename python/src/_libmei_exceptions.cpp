cdc5761c80b4b1f50ad4af8fb4101be9e4bc6268
Andrew Hankinson <andrew.hankinson@mail.mcgill.ca>
2012-10-01 10:08:29 +0200

    Fix pymei copyright attribution

--- python/src/_libmei_exceptions.cpp
+++ python/src/_libmei_exceptions.cpp
@@ -1,5 +1,5 @@
 /*    
-    Copyright (c) 2011-2012 Andrew Hankinson
+    Copyright (c) 2011-2012 Andrew Hankinson, Alastair Porter, Greg Burlet and others
     
     Permission is hereby granted, free of charge, to any person obtaining
     a copy of this software and associated documentation files (the
