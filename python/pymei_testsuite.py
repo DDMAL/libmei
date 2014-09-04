103eae78567ae0789a5bee97767ca4342715629b
Andrew Hankinson <andrew.hankinson@gmail.com>
2014-09-04 15:52:20 -0400

    New: MeiAttribute Python Unit test

--- python/pymei_testsuite.py
+++ python/pymei_testsuite.py
@@ -1,16 +1,19 @@
 import unittest
 from test import meielement_test
 from test import meidocument_test
+from test import meiattribute_test
 from test import xmlimport_test
 from test import xmlexport_test
+
 
 def suite():
     test_suite = unittest.TestSuite()
     test_suite.addTest(meielement_test.suite())
     test_suite.addTest(meidocument_test.suite())
+    test_suite.addTest(meiattribute_test.suite())
     test_suite.addTest(xmlimport_test.suite())
     test_suite.addTest(xmlexport_test.suite())
     return test_suite
-    
+
 runner = unittest.TextTestRunner()
 runner.run(suite())
