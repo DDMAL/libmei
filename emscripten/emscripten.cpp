8d22cec2e21785b21277982e5334a629b9e05319
Andrew Hankinson <andrew.hankinson@gmail.com>
2015-04-11 15:23:24 -0400

    WIP: Updating some emscripten bindings

--- emscripten/emscripten.cpp
+++ emscripten/emscripten.cpp
@@ -9,6 +9,7 @@
 
 #include "meidocument.h"
 #include "meielement.h"
+#include "meiattribute.h"
 #include "import/xmlimport.h"
 
 using namespace mei;
@@ -16,15 +17,32 @@ using namespace emscripten;
 
 // Binding code
 EMSCRIPTEN_BINDINGS(libmei) {
+    register_vector<MeiAttribute*>("MeiAttributeList");
+
     class_<MeiDocument>("MeiDocument")
     .constructor<std::string>()
+//    .constructor<>()
     .function("getRootElement", &MeiDocument::getRootElement, allow_raw_pointers())
     .function("getVersion", &MeiDocument::getVersion)
+    ;
+    
+    class_<MeiAttribute>("MeiAttribute")
+    .constructor<std::string, std::string>()
+    .function("getName", &MeiAttribute::getName)
+    .function("getValue", &MeiAttribute::getValue)
+    .function("getElement", &MeiAttribute::getElement, allow_raw_pointers())
     ;
     
     class_<MeiElement>("MeiElement")
     .constructor<std::string>()
     .function("getId", &MeiElement::getId)
+    .function("getName", &MeiElement::getName)
+    .function("getTail", &MeiElement::getTail)
+    .function("getValue", &MeiElement::getValue)
+    .function("getAttributes", &MeiElement::getAttributes, allow_raw_pointers())
+//    .function("addAttribute", &MeiElement::addAttribute)
+    .function("setAttributes", &MeiElement::setAttributes)
+    .function("removeAttribute", &MeiElement::removeAttribute)
     ;
     
     class_<XmlImport>("XmlImport")
