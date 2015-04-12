43505a4da4a6aba32eee493f2c6d845a17cabc99
Laurent Pugin <lxpugin@gmail.com>
2015-04-12 17:45:54 +0200

    Add tests for overloaded addAttribute method on MeiElement

--- emscripten/test/meielement.spec.js
+++ emscripten/test/meielement.spec.js
@@ -7,5 +7,21 @@ describe('libmei', function()
             var element = new Module.MeiElement("note");
             expect(element.getName()).toBe("note");
         });
+        
+        it("should add a 'pname' attribute as string with value 'c'", function()
+        {
+            var element = new Module.MeiElement("note");
+            element.addAttribute("pname", "c");
+            var attribute = element.getAttribute("pname");
+            expect(attribute.getValue()).toBe("c");
+        });
+        
+        it("should add a 'pname' attribute as MEI Attribute with value 'c'", function()
+        {
+            var element = new Module.MeiElement("note");
+            element.addAttribute(new Module.MeiAttribute("pname", "c"));
+            var attribute = element.getAttribute("pname");
+            expect(attribute.getValue()).toBe("c");
+        });
     });
 });
