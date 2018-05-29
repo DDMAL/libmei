f68f65661b3c1dd888e449ece7dbe62aab9e1703
Noah Baxter <noah.baxter@mail.mcgill.ca>
2018-05-29 11:25:39 -0400

    Fixed to build/install succesfully for python 2.7 or python 3.6

--- python/setup.py
+++ python/setup.py
@@ -7,7 +7,12 @@ longdesc = """ Python Bindings for LibMEI. """
 
 if sys.platform == "darwin":
     link_args = ["-F", "/Library/Frameworks","-framework", "mei"]
-    libraries = ["boost_python36-mt"]
+    if sys.version_info.major == 2:
+        libraries = ["boost_python27-mt"]
+    elif sys.version_info.major == 3:
+        libraries = ["boost_python36-mt"]
+    else:
+        libraries = ["boost_python-mt"]
     library_dirs = []
     runtime_library_dirs = []
     include_dirs = []
